23de71c54cbffbe374a5e62387139a608e07d304
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-11-13 20:48:00 -0500

    feature: added reducer useNewSubForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ListSubs, Form } from './components';
 import './index.css'
 import { INITIAL_STATE } from './constants';
@@ -10,21 +10,37 @@ interface AppState {
 }
 
 function App() {
+  // BP: Es buena práctica no pasar directamente el setSubs del state, así no nos acostumbramos a utilizar el namespace React para todos los tipos. 
   const [ subs, setSubs ] = useState<AppState['subs']>([])
 
   useEffect(() => {
     setSubs(INITIAL_STATE)
   },[])
+
+  // El useRef suele ser problematico, una forma de solucionarlo es tiparlo de la siguiente forma 
+  const divRef = useRef<HTMLDivElement>(null)
+  // Lo anterior evitará que al pasarlo genere errores de mutabilidad, es importante que se establezca un valor inicial, como en el caso anterior, que se definió null
+
+  // Para evitar pasar el set del state podemos realizar lo siguiente
+  const handleNewSub = ( newSub : Sub ): void => {
+    setSubs( subs => ([ ...subs, newSub ]))
+  }
   
   return (
-    <div className="App bg-hero min-h-screen font-spaceGrotesk">
+    <div className="App bg-hero min-h-screen font-spaceGrotesk" ref={ divRef }>
       <header className='bg-gray-800 text-gray-100 py-10 flex-center'>
         <h1 className='text-4xl font-bold text-center text-greenLight uppercase' >Luissdev subs</h1>
       </header>
 
       <main className='container mx-auto py-5'>
         <ListSubs subs={ subs } />
+        {/* Evitar pasar directamente el set del state
         <Form onNewSub={ setSubs }/>
+
+        Mejor practica es
+        */}
+        <Form onNewSub={ handleNewSub} />
+
       </main>
       <footer className='flex-center flex-col py-3 footer'>
         <p className='text-center text-greenLight'>Todos los derechos reservados</p>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,45 +1,58 @@
-import React, { useState } from "react"
 import { Sub } from "../constants/types"
-
-interface FormValues {
-  inputValues : Sub
-}
+import useNewSubForm from "../hooks/useNewSubForm"
 
 interface FormProps {
-  onNewSub: React.Dispatch<React.SetStateAction<Sub[]>>
+  onNewSub: ( newSub : Sub ) => void
 }
-
 function Form({ onNewSub } : FormProps) {
 
-  const [ inputValues, setInputValues ] = useState<FormValues['inputValues']>({
-    nick: '',
-    subMonths: 0,
-    avatar: '',
-    description: ''
-  })
+  /* const [ inputValues, setInputValues ] = useState<FormState['inputValues']>(INITIAL_STATE) */
+
+  const [ inputValues, distpach ] = useNewSubForm()
+
+  const handleClearForm = () => {
+    distpach({
+      type: "clear"
+    })
+  }
 
   // Manejor de eventos
   const handleSubmit = (e : React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    onNewSub( subs => ([ ...subs, inputValues]))
+    onNewSub( inputValues )
+    handleClearForm()
   }
 
   // BP: Interesante práctica para encontrar el tipo de evento que se recibe ya que este cambia dependiendo del contexto. Para realizar esto se paso la funcion del handleChange al onChange del input y acciendo hover sobre la variable del evento se puede ver el tipo de evento que se recibe 
+  /*
   const handleChange = (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setInputValues({
       ...inputValues,
       [e.target.name] : e.target.value
     })
+  */ 
+  // Ahora con reducer
+  const handleChange = (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    distpach({
+      type: "change_value",
+      payload: {
+        inputName: e.target.name,
+        inputValue: e.target.value
+      }
+    })
 }
 
   return (
-    <form onSubmit={ handleSubmit } className="md:max-w-md mx-auto">
-      <input onChange={ handleChange } value={ inputValues.nick } className="input" type='text' name='nick' placeholder='nick' />
-      <input onChange={ handleChange } value={ inputValues.subMonths } className="input" type='text' name='subMonths' placeholder='subMonths' />
-      <input onChange={ handleChange } value={ inputValues.avatar } className="input" type='text' name='avatar' placeholder='avatar' />
-      <textarea onChange={ handleChange } value={ inputValues.description } className="input" name='description' placeholder='description' />
-      <button className="btn">Save new sub!</button>
-    </form>
+    <>
+      <form onSubmit={ handleSubmit } className="md:max-w-md mx-auto">
+        <input onChange={ handleChange } value={ inputValues.nick } className="input" type='text' name='nick' placeholder='nick' />
+        <input onChange={ handleChange } value={ inputValues.subMonths } className="input" type='text' name='subMonths' placeholder='subMonths' />
+        <input onChange={ handleChange } value={ inputValues.avatar } className="input" type='text' name='avatar' placeholder='avatar' />
+        <textarea onChange={ handleChange } value={ inputValues.description } className="input" name='description' placeholder='description' />
+        <button type="submit" className="btn">Save new sub!</button>
+        <button type="button" onClick={ handleClearForm } className="btn">Clear</button>
+      </form>
+    </>
   )
 }
 
